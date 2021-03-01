8da9efa074d4cdb9d60e9086e46f0f713f646221
Ben <benm4643@gmail.com>
2021-02-28 20:25:07 -0500

    Created Project

---
+++ app/src/main/java/edu/uc/mille5bm/smartnutrition/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.uc.mille5bm.smartnutrition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
