4b6142accec6c509cc870b1672e6bc9928e779e4
Mk <furtermm@mail.uc.edu>
2021-03-04 18:17:40 -0500

    UI- added necessary instance of View.OnClickListener and wires the listener to the button using setOnClickListener(View.OnClickListener) for all 3 activities.

--- app/src/main/java/edu/uc/mille5bm/smartnutrition/MainActivity.kt
+++ app/src/main/java/edu/uc/mille5bm/smartnutrition/MainActivity.kt
@@ -1,11 +1,16 @@
 package edu.uc.mille5bm.smartnutrition
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.button1).setOnClickListener { startActivity(Intent(applicationContext, NutritionInfoActivity::class.java)) }
+        findViewById<View>(R.id.button2).setOnClickListener { startActivity(Intent(applicationContext, CalorieCounterActivity::class.java)) }
+        findViewById<View>(R.id.button3).setOnClickListener { startActivity(Intent(applicationContext, HealthTrackerActivity::class.java)) }
     }
 }
