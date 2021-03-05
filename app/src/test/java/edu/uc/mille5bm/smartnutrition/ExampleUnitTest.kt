e1bd3c04153b3f8b85b2c5a320b054f9b84e4eee
bodensjr <70085015+bodensjr@users.noreply.github.com>
2021-03-04 22:03:32 -0500

    Update ExampleUnitTest.kt
    There weren't any unit tests in the project. A lack of unit tests is one of the Developers' Seven Deadly Sins. I understand that there isn't currently enough code to do proper unit tests, but it would still be a good idea to have a baseline for when you are able to do unit tests, since what you have in the unit test files doesn't affect the rest of the project.

--- app/src/test/java/edu/uc/mille5bm/smartnutrition/ExampleUnitTest.kt
+++ app/src/test/java/edu/uc/mille5bm/smartnutrition/ExampleUnitTest.kt
@@ -14,4 +14,14 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+    
+    @Test
+    fun confirmExercise(){
+        var workout = Exercise() 
+    }
+    
+    @Test
+    fun confirmFood(){
+    var munchies = Food() 
+    }
 }
