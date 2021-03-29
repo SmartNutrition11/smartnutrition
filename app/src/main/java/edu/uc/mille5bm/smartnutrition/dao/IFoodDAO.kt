fc7741d458d68c14e9fb3c861a99d94dd0262d51
TonyTheMaccaroni <57689300+TonyTheMaccaroni@users.noreply.github.com>
2021-03-28 20:52:00 -0400

    Added IFoodDAO

--- app/src/main/java/edu/uc/mille5bm/smartnutrition/dao/IFoodDAO.kt
+++ app/src/main/java/edu/uc/mille5bm/smartnutrition/dao/IFoodDAO.kt
@@ -1,4 +1,11 @@
 package edu.uc.mille5bm.smartnutrition.dao
 
-class IFoodDAO {
+import  edu.uc.mille5bm.smartnutrition.dto.Food
+import  retrofit2.Call
+import retrofit2.http.GET
+
+interface IFoodDAO {
+
+    @GET("https://github.com/terrenjpeterson/caloriecounter/blob/master/src/data/foods.json")
+    fun getAllFoods(): Call<ArrayList<Food>>
 }
