e4deb63511f10c316774a7c79ac9ff41e3a6bb34
Mk <furtermm@mail.uc.edu>
2021-03-30 03:26:35 -0400

    Added Activity Tracker UI and a few functionality.

--- app/src/main/java/edu/uc/mille5bm/smartnutrition/CalorieCounterActivity.kt
+++ app/src/main/java/edu/uc/mille5bm/smartnutrition/CalorieCounterActivity.kt
@@ -1,11 +1,35 @@
+/**This will allow user to enter their activity**/
 package edu.uc.mille5bm.smartnutrition
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class CalorieCounterActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nutrition_info)
+        setContentView(R.layout.activity_tracker)
+        var listItems = ArrayList<String>()
+        var adapter: ArrayAdapter<String>? = null
+        var clickCounter = 0
+
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                listItems)
+    }
+    fun addItems(v: View?) {
+
+        findViewById<View>(R.id.llAddActivity).visibility = View.VISIBLE
+    }
+    fun submit(v: View?) {
+        findViewById<View>(R.id.llAddActivity).visibility = View.GONE
+        var ed1 = findViewById<EditText>(R.id.etName)
+        val ed2 = findViewById<EditText>(R.id.etCal)
+        //listItems.add(ed1.text.toString() + ",  Calories Burned: " + ed2.text)
+      //  adapter!!.notifyDataSetChanged()
+        ed1.setText("")
+        ed2.setText("")
     }
 }
