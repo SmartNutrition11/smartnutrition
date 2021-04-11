69f787d86ec438bc4d4cb4238d072a2f99ff3b56
TonyTheMaccaroni <57689300+TonyTheMaccaroni@users.noreply.github.com>
2021-04-11 18:06:00 -0400

    Modified Activity Tracker screen

--- app/src/main/java/edu/uc/mille5bm/smartnutrition/CalorieCounterActivity.kt
+++ app/src/main/java/edu/uc/mille5bm/smartnutrition/CalorieCounterActivity.kt
@@ -8,27 +8,21 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class CalorieCounterActivity: AppCompatActivity() {
+    val listItems = ArrayList<String>()
+    var adapter: ArrayAdapter<String>? = null
+    val clickCounter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tracker)
-        var listItems = ArrayList<String>()
-        var adapter: ArrayAdapter<String>? = null
-        var clickCounter = 0
-
         adapter = ArrayAdapter(this,
                 android.R.layout.simple_list_item_1,
                 listItems)
     }
     fun addItems(v: View?) {
-
-        findViewById<View>(R.id.llAddActivity).visibility = View.VISIBLE
-    }
-    fun submit(v: View?) {
-        findViewById<View>(R.id.llAddActivity).visibility = View.GONE
         var ed1 = findViewById<EditText>(R.id.etName)
         val ed2 = findViewById<EditText>(R.id.etCal)
-        //listItems.add(ed1.text.toString() + ",  Calories Burned: " + ed2.text)
-      //  adapter!!.notifyDataSetChanged()
+        listItems.add(ed1.text.toString() + ",  Calories Burned: " + ed2.text)
+        adapter!!.notifyDataSetChanged()
         ed1.setText("")
         ed2.setText("")
     }
