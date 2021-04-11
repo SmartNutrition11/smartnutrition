d2e15dca90852aa5ac209345c31cc8eb340868ed
Ben Miller <benm4643@gmail.com>
2021-04-11 10:07:12 -0400

    Revert "Merge pull request #20 from SmartNutrition11/huangtz_codeReview2"
    This reverts commit bd882e4d85406e102e7fc41df9c8a96731151510.

--- app/src/main/java/edu/uc/mille5bm/smartnutrition/MainActivity.kt
+++ app/src/main/java/edu/uc/mille5bm/smartnutrition/MainActivity.kt
@@ -3,14 +3,18 @@ package edu.uc.mille5bm.smartnutrition
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import edu.uc.mille5bm.smartnutrition.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<View>(R.id.btn_nutrition).setOnClickListener { startActivity(Intent(applicationContext, NutritionInfoActivity::class.java)) }
-        findViewById<View>(R.id.btn_caloriepage).setOnClickListener { startActivity(Intent(applicationContext, CalorieCounterActivity::class.java)) }
-        findViewById<View>(R.id.btn_health).setOnClickListener { startActivity(Intent(applicationContext, BMIActivity::class.java)) }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnNutrition.setOnClickListener { startActivity(Intent(applicationContext, NutritionInfoActivity::class.java)) }
+        binding.btnCalorie.setOnClickListener { startActivity(Intent(applicationContext, CalorieCounterActivity::class.java)) }
+        binding.btnHealth.setOnClickListener { startActivity(Intent(applicationContext, BMIActivity::class.java)) }
     }
 }
