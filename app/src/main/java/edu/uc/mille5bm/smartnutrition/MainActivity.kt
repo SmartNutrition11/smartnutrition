f6f773b2fe9c23a609170da3d409e49556b33e72
Ben Miller <benm4643@gmail.com>
2021-04-11 09:56:21 -0400

    Revert "Merge branch 'main' into huangtz_codeReview2"
    This reverts commit f92e6e64398d34485d592cd0d2b98b2c1e30f67b.

--- app/src/main/java/edu/uc/mille5bm/smartnutrition/MainActivity.kt
+++ app/src/main/java/edu/uc/mille5bm/smartnutrition/MainActivity.kt
@@ -3,12 +3,9 @@ package edu.uc.mille5bm.smartnutrition
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import edu.uc.mille5bm.smartnutrition.databinding.ActivityMainBinding
-
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
