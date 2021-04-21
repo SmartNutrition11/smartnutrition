3f7b6601f9803a348305ec24a973c627a662fa19
Ben <benm4643@gmail.com>
2021-04-21 18:15:55 -0400

    Made changes to retro fit and added items to list view

--- app/src/main/java/edu/uc/mille5bm/smartnutrition/NutritionInfoActivity.kt
+++ app/src/main/java/edu/uc/mille5bm/smartnutrition/NutritionInfoActivity.kt
@@ -4,20 +4,27 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import edu.uc.mille5bm.smartnutrition.dto.Food
 import edu.uc.mille5bm.smartnutrition.ui.main.FoodViewModel
+import kotlinx.android.synthetic.main.activity_nutrition_info.*
 
 
 class NutritionInfoActivity: AppCompatActivity() {
 
     lateinit var listView: ListView
-    lateinit var stringArrayList: ArrayList<Food>
+    lateinit var foodArrayList: ArrayList<Food>
     lateinit var arrayAdapter: ArrayAdapter<String>
     private lateinit var viewModel: FoodViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nutrition_info)
+        viewModel = ViewModelProviders.of(this).get(FoodViewModel::class.java)
         listView = findViewById(R.id.nutritionListView)
+        viewModel.foods.observe(this, Observer {
+            foods -> listView.setAdapter(ArrayAdapter(baseContext!!,R.layout.support_simple_spinner_dropdown_item,foods))
+        })
     }
 }
--- app/src/main/java/edu/uc/mille5bm/smartnutrition/dto/Food.kt
+++ app/src/main/java/edu/uc/mille5bm/smartnutrition/dto/Food.kt
@@ -3,7 +3,7 @@ package edu.uc.mille5bm.smartnutrition.dto
 
 import java.util.*
 
-class Food(var id: String, var name: String, var nutritionPer100g: String, var tags: String){
+data class Food(var id: String, var name: String, var nutritionPer100g: String, var tags: Array<String>){
     override fun toString(): String{
         return name
     }
